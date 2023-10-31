394e999532bda9dca22734379ce55e6ba401db31
AsadbekBoqiyev <boqiyevasadbek51@gmail.com>
2023-10-31 19:22:43 +0500

    tugadi shekili

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use stric";
 
 let data = [];
+let id = 0;
 
 // element
 const list = document?.querySelector(".list");
@@ -26,45 +27,15 @@ const createElement = () => {
     data.forEach((d) => {
       list.insertAdjacentHTML(
         "afterbegin",
-        `<li class="list-item ${d.isChecked ? "isChecked" : ""} " data-set=${
+        `<li class="list-item  ${d.isChecked ? "isChecked" : ""}" data-set="${
           d?.id
-        }>
-        <div>
-        ${
-          d.isChecked
-            ? `<span class="checked">
-        <svg
-          width="28"
-          height="28"
-          viewBox="0 0 28 28"
-          style="fill: rgba(10, 239, 61, 1); transform: 0; msfilter: 0"
-        >
-          <path
-            d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm-1.999 14.413-3.713-3.705L7.7 11.292l2.299 2.295 5.294-5.294 1.414 1.414-6.706 6.706z"
-          ></path>
-        </svg>
-      </span>
-`
-            : `<span class="circle">
-<svg
-  width="28"
-  height="28"
-  viewBox="0 0 28 28"
-  style="fill: white; transform: 0; msfilter: 0"
->
-  <path
-    d="M5 12c0 3.859 3.14 7 7 7 3.859 0 7-3.141 7-7s-3.141-7-7-7c-3.86 0-7 3.141-7 7zm12 0c0 2.757-2.243 5-5 5s-5-2.243-5-5 2.243-5 5-5 5 2.243 5 5z"
-  ></path>
-</svg>
-</span>`
-        }
-          
-            <div class="name ${d.isChecked ? "isChecked" : ""}">${
-          d?.listName
-        }</div>
-        </div>
-        <div class="createdAt"><span>${d?.createdAt}</span></div>
-      </li>`
+        }">
+        <span class="check">${d.isChecked ? "checked" : "check"}</span>
+      <p class="name">${d?.listName}</p>
+      <span class="edit-s">edit</span>
+      <span class="del-s">del</span>
+      <div class="createdAt"><span>${d?.createdAt}</span></div>
+    </li>`
       );
     });
   }
@@ -72,43 +43,65 @@ const createElement = () => {
 
 createBtn?.addEventListener("click", (e) => {
   e?.preventDefault();
-  list.innerHTML = "";
-  const date = new Date();
-  const hour = date.getHours();
-  const listObj = {
-    id: date.getTime(),
-    isChecked: false,
-    listName: input?.value,
-    createdAt: `${Math.abs(hour - 12)}.${
-      date.getMinutes() <= 10 ? 0 + date.getMinutes() : date.getMinutes()
-    } ${hour <= 12 ? "AM" : "PM"}`,
-  };
-  data.push(listObj);
+  if (id === 0) {
+    const date = new Date();
+    const hour = date.getHours();
+    const listObj = {
+      id: date.getTime(),
+      isChecked: false,
+      listName: input?.value,
+      createdAt: `${Math.abs(hour - 12)}.${
+        date.getMinutes() <= 10 ? 0 + date.getMinutes() : date.getMinutes()
+      } ${hour <= 12 ? "AM" : "PM"}`,
+    };
+    data.push(listObj);
+  } else {
+    data = data.map((d) => {
+      if (d.id === id) {
+        return {
+          id: d.id,
+          listName: input.value,
+          isChecked: d.isChecked,
+        };
+      } else {
+        return d;
+      }
+    });
+    id = 0;
+  }
+
   createElement();
   input.value = "";
   form.classList.add("hidden");
   plusBtn.classList.remove("hidden");
 });
 
-list.addEventListener("click", () => {
-  listItme = document.querySelectorAll(".list-item");
-  listItme?.forEach((itme) => {
-    itme?.addEventListener("click", (e) => {
-      const dataset = +e.currentTarget.dataset.set;
-      data = data.map((d) => {
-        if (d.id === dataset) {
-          const newObj = {
-            id: d.id,
-            listName: d.listName,
-            createdAt: d.createdAt,
-            isChecked: true,
-          };
-          return newObj;
-        } else {
-          return d;
-        }
-      });
-      createElement();
+list.addEventListener("click", (e) => {
+  console.log("salom");
+  const target = e.target.classList[0];
+  const dataset = +e.target.parentElement.dataset.set;
+  if (target === "check") {
+    data = data.map((d) => {
+      if (d.id === dataset) {
+        const newObj = {
+          id: d.id,
+          listName: d.listName,
+          createdAt: d.createdAt,
+          isChecked: !d.isChecked,
+        };
+        return newObj;
+      } else {
+        return d;
+      }
     });
-  });
+    createElement();
+  } else if (target === "del-s") {
+    data = data.filter((d) => d.id !== dataset);
+    createElement();
+  } else if (target === "edit-s") {
+    form?.classList.remove("hidden");
+    plusBtn?.classList.add("hidden");
+    input.value = data.filter((d) => d.id === dataset)[0].listName;
+    id = dataset;
+  }
 });
